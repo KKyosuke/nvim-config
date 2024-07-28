0acfc55df50921e2f5ebe684cbe28b100bda65ce
kyosuke <kyosuke.k.94@gmail.com>
2024-07-29 08:12:27 +0900

    init

---
+++ init.lua
@@ -0,0 +1,7 @@
+-- ===== lua script ======
+require("options")
+require("plugins")
+require("keymaps")
+require("autocmds")
+
+vim.cmd[[colorscheme evening]]
---
+++ lua/options.lua
@@ -0,0 +1,21 @@
+local opt = vim.opt
+
+-- 行番号の表示
+opt.number = true
+
+-- タブとインデントの設定
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+
+-- 検索設定
+opt.ignorecase = true
+opt.smartcase = true
+
+-- cursors
+opt.cursorline = true
+opt.cursorcolumn = true
+
+-- search
+opt.hlsearch = true
+opt.incsearch = true
